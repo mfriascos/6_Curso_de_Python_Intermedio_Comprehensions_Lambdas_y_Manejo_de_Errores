5f88ee9fbbdc992c43446df7af4935efeb035df8
Mario <marioriascos1201@gmail.com>
2022-10-02 13:22:04 -0500

    Dictionary Comprehensions

--- proyecto_2/dict_comprehensions.py
+++ proyecto_2/dict_comprehensions.py
@@ -11,7 +11,14 @@ def run():
     #         my_dict[i] = i**3
 
 # Solución con Dictionary Comprehensions
-    my_dict = { i:i**3 for i in range(1,101) if i%3 != 0}
+    
+    # my_dict = { i:i**3 for i in range(1,101) if i%3 != 0}
+    # print(my_dict)
+
+# Reto: Crear, con un dictionary Comprehension, un diccionario cuyas llaves sean los 1000 primeros números
+# naturales con sus raíces cuadradas como valores. 
+
+    my_dict = {i:i**0.5 for i in range(1,1001)}
     print(my_dict)
 
 
