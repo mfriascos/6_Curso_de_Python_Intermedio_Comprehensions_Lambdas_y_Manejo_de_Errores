4f007c90bb840c13cec1d6852f34b48c47141799
Mario <marioriascos1201@gmail.com>
2022-10-02 11:29:22 -0500

    List and Dicts

--- proyecto_2/lists_and_dicts.py
+++ proyecto_2/lists_and_dicts.py
@@ -17,6 +17,10 @@ def run():
 
     for key, value in super_dict.items():
         print(key, "-", value) 
+    
+    for values in super_list:
+        for key,value in values.items():
+            print(f'{key} - {value}')
 
 if __name__ == '__main__':
     run()
